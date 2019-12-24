79bffab28a8dbdefb0af049956ced023eb635a79
Borden Chen <bordenchen@gmail.com>
2019-12-24 09:19:27 +0000

    configuration

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,10 @@
         ym_web_demo
       </h1>
       <h2 class="subtitle">
-        My stylish Nuxt.js project
+        My dandy Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
